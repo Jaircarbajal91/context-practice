62e762be7c722fdec4df0d90036b7de859fa46f0
Jair Carbajal <jair.carbajal91@gmail.com>
2022-05-04 11:08:33 -0700

    Implemented Car class and Calculator class with passing tests

--- problems/02-calculator.js
+++ problems/02-calculator.js
@@ -1,5 +1,25 @@
 // Your code here
-
+class Calculator {
+	constructor(total = 0) {
+		this.total = total;
+	}
+	add(num) {
+		this.total = num + this.total;
+		return this.total
+	}
+	subtract(num) {
+		this.total = this.total - num;
+		return this.total;
+	}
+	multiply(num) {
+		this.total = this.total * num;
+		return this.total;
+	}
+	divide(num) {
+		this.total = this.total / num;
+		return this.total;
+	}
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
